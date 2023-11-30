19f79d51c2c633c325631e688e455911ce07e705
aangulo <aangulocr@gmail.com>
2023-11-29 20:52:47 -0600

    Actualizando Modelo

---
+++ Portaviones/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Portaviones.MvcApplication" Language="C#" %>
---
+++ Portaviones/Models/Ingreso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portaviones.Models
+{
+    public class Ingreso
+    {
+
+        public int MyProperty { get; set; }
+    }
+}
