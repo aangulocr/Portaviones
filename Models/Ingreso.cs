9f8a4ca20f8329abe491a22434e61b53c2bdbd33
aangulo <aangulocr@gmail.com>
2023-12-10 10:15:47 -0600

    Actualizacion Clases

---
+++ Models/Ingreso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portaviones.Models
+{
+    public class Ingreso
+    {
+
+        public int Serie { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string NombreFantasia { get; set; }
+
+        public float DimensionAla { get; set; }
+
+        public float Alto { get; set; }
+
+        public float Largo { get; set; }
+
+        public float DistanciaVuelo { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Tecnico { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
---
+++ Models/Retiro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portaviones.Models
+{
+    public class Retiro
+    {
+        public int Id { get; set; }
+
+        public string Serie { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string NombreFantasia { get; set; }
+
+        public string Detalle { get; set; }
+
+        public string Tecnico  { get; set; }
+    }
+}
