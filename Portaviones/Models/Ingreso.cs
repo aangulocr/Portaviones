5b608286b6cb00d0eae97f9864dfdb6eea5b8fca
aangulo <aangulocr@gmail.com>
2023-12-10 23:44:45 -0600

    Actualizando clase Retiro

--- Portaviones/Models/Ingreso.cs
+++ Portaviones/Models/Ingreso.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,30 @@ namespace Portaviones.Models
     public class Ingreso
     {
 
-        public int MyProperty { get; set; }
+        public int Serie { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string NombreFantasia { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public Decimal DimensionAla { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public Decimal Alto { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public Decimal Largo { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public Decimal DistanciaVuelo { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Tecnico { get; set; }
+
+        public bool Activo { get; set; }
     }
 }
